383fee1a8af2d8695787525e0fbbb58e04e2956c
patrikduch <duchpatrik@icloud.com>
2025-01-19 11:22:00 +0100

    feat: LangChain code prompt example

--- controllers/langchain_controller.py
+++ controllers/langchain_controller.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 from models.prompt_request import PromptRequest
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 router = APIRouter()
 
@@ -33,4 +34,30 @@ async def get_langchain(request: PromptRequest):
     except Exception as e:
         print("Error:", e)
         raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
+    
+
+@router.post("/code-prompt")
+async def get_langchain_code_prompt():
+    try:
+
+        code_prompt = PromptTemplate(
+            template="Write a very short {language} function that will {task}",
+            input_variables=["language", "task"]
+        )
+
+        code_chain = LLMChain(
+            llm=model,
+            prompt=code_prompt
+        )
+
+        result = code_chain({
+            "language": "python",
+            "task": "return a list of numbers"
+        })
+
+        return {"response": result["text"]}
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
+    
     
