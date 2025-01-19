faf04cdd4b721b66cd3f76f8f17f65d9de7dad96
patrikduch <duchpatrik@icloud.com>
2025-01-19 09:36:13 +0100

    feat: CORS configuration

--- controllers/langchain_controller.py
+++ controllers/langchain_controller.py
@@ -1,30 +1,38 @@
 import os
 from dotenv import load_dotenv
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from langchain.chat_models import ChatOpenAI
 
 router = APIRouter()
 
 load_dotenv()  # Load environment variables from .env file
 
-
 # Initialize the ChatOpenAI model
 model = ChatOpenAI(
 
-    model_name="gpt-3.5-turbo",  # Specify the model
+    model_name="chatgpt-4o-latest",  # Specify the model
     temperature=0.8,            # Creativity level
     max_tokens=700,             # Response length
     verbose=True,                # Logs for debugging
     openai_api_key=os.getenv("OPENAI_API_KEY")
 )
 
-@router.get("/")
-async def get_langchain():
-      # Example query
-    query = "Give me 4 good books to read"
 
-    # Get the response from the model
-    response = await model.apredict(query)
-    print("Response:", response)
+class PromptRequest(BaseModel):
+    prompt: str
 
-    return response
+@router.post("/")
+async def get_langchain(request: PromptRequest):
+    try:
+        # Get the prompt from the request body
+        prompt = request.prompt
+
+        # Get the response from the model
+        response = await model.apredict(prompt)
+        print("Response:", response)
+
+        return {"response": response}
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="An error occurred while processing the request.")
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  
 from controllers import langchain_controller
 
 app = FastAPI()
 
 
+# Allowed origins
+origins = [
+    "http://localhost",
+    "https://localhost:3000", 
+]
+
+
 # Include routers from different controllers
 app.include_router(langchain_controller.router, prefix="/langchain", tags=["LangChain"])
 
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,  # Allow credentials like cookies
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 DEBUG = os.getenv('DEBUG', 'False').lower() in ['true', '1', 't']
 
